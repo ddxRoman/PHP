46247fb34dae0d9a8a6a894f00545d7e423c0e79
ddxRoman <ddxman@mail.ru>
2022-04-26 23:32:03 +0300

    Игрался с Аккордионом, просто добавлено сюда.

--- blocks/Grids/index.php
+++ blocks/Grids/index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <head>
+<meta charset="UTF-8">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,16 +9,26 @@
     <link rel="icon" type="image" href="https://icon2.cleanpng.com/20180329/ike/kisspng-computer-icons-editing-button-clip-art-delete-button-5abced1ac7c040.5271022415223309068182.jpg">
 </head>
 <body>
-        
-<div class="container">
-<div class="item header"><a href="/index.php"><button ><span>BACK</span></button></a>
-    <a href="/blocks/grids/index.php"><button class="ref"><font class="txtbtn">G r i d s </font></button></a></div>
-<div class="item sidebar">Тут меню навигации</div>
-<div class="item content"> <a href="Html&CSS/home.php">Верстка</a><br>
-<iframe class="freym" name="1" src="">
-</iframe></div>
-<div class="item footer">Просто футер, подвал</div>
+<div class="acor-container"> 
+    
+    <input type="radio" name="acor" id="acor1" checked="checked" />
+    <label for="acor1">Заголовок вкладки</label>
+    <div class="acor-body">
+        <p>Описание вкладки</p>
+    </div>
+    
+    <input type="radio" name="acor" id="acor2" />
+    <label for="acor2">Заголовок вкладки</label>
+    <div class="acor-body">
+        <p>Описание вкладки</p>
+    </div>
+    
+    <input type="radio" name="acor" id="acor3" />
+    <label for="acor3">Заголовок вкладки</label>
+    <div class="acor-body">
+        <p>Описание вкладки</p>
+    </div> 
+    
 </div>
-
 </body>
 </html>
