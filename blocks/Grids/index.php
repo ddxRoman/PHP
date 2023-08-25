8daad27f968c8248bebba455c306bac4f73ef952
DDX <ddxman@mail.ru>
2023-08-25 12:45:06 +0300

    Работа над стилем

--- blocks/Grids/index.php
+++ blocks/Grids/index.php
@@ -1,34 +1,22 @@
 <!DOCTYPE html>
+<html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Grid CSS</title>
     <link rel="stylesheet" type="text/css" href="style.css">
-    <link rel="icon" type="image" href="https://icon2.cleanpng.com/20180329/ike/kisspng-computer-icons-editing-button-clip-art-delete-button-5abced1ac7c040.5271022415223309068182.jpg">
-    <meta http-equiv="Content-Type" content="text/html; charset=windows-1251"> 
+    <title>Grid</title>
 </head>
 <body>
-<div class="acor-container"> 
-    
-    <input type="radio" name="acor" id="acor1" checked="checked" />
-    <label for="acor1">Заголовок вкладки</label>
-    <div class="acor-body">
-        <p>Описание вкладки</p>
-    </div>
-    
-    <input type="radio" name="acor" id="acor2" />
-    <label for="acor2">Заголовок вкладки</label>
-    <div class="acor-body">
-        <p>Описание вкладки</p>
-    </div>
-    
-    <input type="radio" name="acor" id="acor3" />
-    <label for="acor3">Заголовок вкладки</label>
-    <div class="acor-body">
-        <p>Описание вкладки</p>
-    </div> 
-    
+<div class="full_page">
+<div class="header"><h1>CSS Grid</h1></div>
+<div class="NavBar">
+    <button>Function 1</button>
+    <button>Function 2</button>
+    <button>Function 3</button>
+    <button>Function 4</button>
+</div>
+<div class="container">1. Делаем прилепающий футер к низу экрана<br>2. Растягиваем контент ауто<br>3.С кнопками что то решить надо по размеру их</div>
+<div class="footer">ORS Studio<br>Lessons in developer<br>2022-2023</div>
 </div>
 </body>
 </html>
